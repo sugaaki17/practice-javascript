76e366d71245862447d5f4b4268af36185dbc20c
rio7700 <riolog1234@gmail.com>
2024-09-20 04:52:00 +0000

    9月19日分の別をファイルに移動した

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector("h1").textContent = "そのだりお"
+
+//documentとは！！
+/*htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+//DOM（ドむ）
+/*ドキュメントオブジェクトモデル（Document Object Model）の略で、
+WEBページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+//上記コードの意味
+//document.queryselector("h1").textContent="自分の名前"
+
+//ドキュメントの中の("h1")タグを探してね
+//document.queryselector("h1")
+
+//h1タグの中身
