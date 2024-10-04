2ae1b0f3aca84754acff996701a529ba42d4e834
rio7700 <riolog1234@gmail.com>
2024-10-04 06:00:12 +0000

    変更

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-function changeColor(){
-document.getElementById("text").style.color="red";
+function errorInput(){
+    const month=document.getElementById("month").value;
+    const day=document.getElementById("day").value;
+    if(month<1||month>12){
+        alert("適切な値ではありません")
+        return
+    }    
+
+    console.log("Month:",month,"Day:",day)
+
 }
 
 
+//document.getElementByid("month")はタグを確認するだけ
+//valueはそのタグの値を示す
 
 
-//docment.getElementIdById("text")style.color="red";
-/*これは、ドキュメントの中の｛text｝というIDを持っている
-タグのスタイルの色を赤にしてください*/
-
-
-
-//document.querySelectorAll("h1").style.color="red";
-let count=0;
-function increaseCount(){
-
-count++;
-document.getElementById("counter").innerText=count;
-}
-function herasukaunto(){
-
-count--;
-document.getElementById("counter").innerText=count;
-}
+/*
+javaScriptのプログラムとHTMLファイルはどこで対応しているかタグについているIDで紐ずく
+タグの中にある付加情報のことを属性という
+console.log()はｃ言語でいうところのprintf();です*/
